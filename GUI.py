b38fd30bd60cf40f146886a40465aa2ce920f5bf
Ruocheng An <101235394+RRRRRAVENNNNN@users.noreply.github.com>
2024-07-02 15:29:09 +0800

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,36 @@
+import joblib
+import xgboost as xgb
+import numpy as np
+import streamlit as st
+
+import streamlit as st
+
+st.image('JLU.png')
+
+
+st.header('ML model for predicting thermal conductivity of bentonite')
+
+model = joblib.load('ML model.joblib')
+ss = joblib.load('StandardScaler.joblib')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    feature1 = st.number_input(u'$\mathrm{Temperature}$', step=0.01, format='%.2f')
+    feature2 = st.number_input(u'$\mathrm{Water\;content}$', step=0.01, format='%.2f')
+    feature3 = st.number_input(u'$\mathrm{Dry\;density}$', step=0.01, format='%.2f')
+    feature4 = st.number_input(u'$\mathrm{Montmorillonite\;content}$', step=0.01, format='%.2f')
+
+with col2:
+    feature5 = st.number_input(u'$\mathrm{Liquid\;limit}$', step=0.01, format='%.2f')
+    feature6 = st.number_input(u'$\mathrm{Plastic\;limit}$', step=0.01, format='%.2f')
+    feature7 = st.number_input(u'$\mathrm{Graphite\;content}$', step=0.01, format='%.2f')
+    feature8 = st.number_input(u'$\mathrm{Sand\;content}$', step=0.01, format='%.2f')
+    
+feature_values = [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8]
+
+if st.button('Predict', type='primary'):
+    input_data = np.array([feature_values])
+    input_data_scaled = ss.transform(input_data)
+    prediction = model.predict(input_data_scaled)
+    st.success(f'Predicted thermal conductivity: {prediction[0]:.2f} W/mK', icon="✅")
